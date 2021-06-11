c42e27037e86f6d72f0c8f09f7fec2ea85755134
handuah <handuah@gmail.com>
2021-06-11 13:14:19 +0000

    initial commit

---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:sizer/sizer.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class HomeScreen extends StatefulWidget {
+  // const HomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: HexColor("#FFFFFF"),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.02),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
