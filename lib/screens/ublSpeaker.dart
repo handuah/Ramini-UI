9777a1198f05eb0e5908a6d2324e631903528a23
handuah <handuah@gmail.com>
2021-06-14 13:19:05 +0000

    finished with buttons

--- lib/screens/ublSpeaker.dart
+++ lib/screens/ublSpeaker.dart
@@ -31,7 +31,9 @@ class _UBLSpeakersState extends State<UBLSpeakers> {
               children: [
                 IconButton(
                   icon: Icon(Icons.arrow_back),
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
                 IconButton(
                   icon: Icon(Icons.favorite_outline),
@@ -113,6 +115,228 @@ class _UBLSpeakersState extends State<UBLSpeakers> {
                       ),
                     ),
                   ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            DefaultTabController(
+              length: 4, // length of tabs
+              initialIndex: 0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+                    child: TabBar(
+                      labelColor: HexColor("#F74463"),
+                      unselectedLabelColor: Colors.black,
+                      tabs: [
+                        Tab(
+                          child: Text(
+                            'Description',
+                            style: GoogleFonts.alata(
+                              textStyle: TextStyle(
+                                // letterSpacing: 2,
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.w500,
+                                // color: HexColor("#F74463"),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Tab(
+                          child: Text(
+                            'Reviews',
+                            style: GoogleFonts.alata(
+                              textStyle: TextStyle(
+                                // letterSpacing: 2,
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.w500,
+                                // color: HexColor("#F74463"),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Tab(
+                          child: Text(
+                            'Ofers',
+                            style: GoogleFonts.alata(
+                              textStyle: TextStyle(
+                                // letterSpacing: 2,
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.w500,
+                                // color: HexColor("#F74463"),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Tab(
+                          child: Text(
+                            'Policy',
+                            style: GoogleFonts.alata(
+                              textStyle: TextStyle(
+                                // letterSpacing: 2,
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.w500,
+                                // color: HexColor("#F74463"),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                    decoration: BoxDecoration(
+                      border: Border(
+                          top: BorderSide(color: Colors.grey, width: 0.5)),
+                    ),
+                    child: TabBarView(
+                      children: [
+                        Container(
+                          child: Text(
+                            'Take your tunes on the go with the powerful JBL Flip 5. Our lightweight Bluetooth speaker goes anywhere. Bad weather? Not to worry. With its waterproof design, you can rock out to our signature sound rain or shine. Move more. Pair two JBL PartyBoost-compatible speakers together for stereo sound or link multiple JBL PartyBoost-compatible speakers to create a bigger party. Enjoy up to 12 hours of play time for your favorite music. Stand it vertical or horizontal and be bold with your choice of 11 vibrant colors.',
+                            style: GoogleFonts.alata(
+                              textStyle: TextStyle(
+                                // letterSpacing: 2,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.normal,
+                                // color: HexColor("#F74463"),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            'Take your tunes on the go with the powerful JBL Flip 5. Our lightweight Bluetooth speaker goes anywhere. Bad weather? Not to worry. With its waterproof design, you can rock out to our signature sound rain or shine. Move more. Pair two JBL PartyBoost-compatible speakers together for stereo sound or link multiple JBL PartyBoost-compatible speakers to create a bigger party. Enjoy up to 12 hours of play time for your favorite music. Stand it vertical or horizontal and be bold with your choice of 11 vibrant colors.',
+                            style: GoogleFonts.alata(
+                              textStyle: TextStyle(
+                                // letterSpacing: 2,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.normal,
+                                // color: HexColor("#F74463"),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            'Take your tunes on the go with the powerful JBL Flip 5. Our lightweight Bluetooth speaker goes anywhere. Bad weather? Not to worry. With its waterproof design, you can rock out to our signature sound rain or shine. Move more. Pair two JBL PartyBoost-compatible speakers together for stereo sound or link multiple JBL PartyBoost-compatible speakers to create a bigger party. Enjoy up to 12 hours of play time for your favorite music. Stand it vertical or horizontal and be bold with your choice of 11 vibrant colors.',
+                            style: GoogleFonts.alata(
+                              textStyle: TextStyle(
+                                // letterSpacing: 2,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.normal,
+                                // color: HexColor("#F74463"),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            'Take your tunes on the go with the powerful JBL Flip 5. Our lightweight Bluetooth speaker goes anywhere. Bad weather? Not to worry. With its waterproof design, you can rock out to our signature sound rain or shine. Move more. Pair two JBL PartyBoost-compatible speakers together for stereo sound or link multiple JBL PartyBoost-compatible speakers to create a bigger party. Enjoy up to 12 hours of play time for your favorite music. Stand it vertical or horizontal and be bold with your choice of 11 vibrant colors.',
+                            style: GoogleFonts.alata(
+                              textStyle: TextStyle(
+                                // letterSpacing: 2,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.normal,
+                                // color: HexColor("#F74463"),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Material(
+                  elevation: 4.0,
+                  borderRadius: BorderRadius.circular(50.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.08,
+                    width: MediaQuery.of(context).size.width / 2.4,
+                    decoration: BoxDecoration(
+                      color: HexColor("#F74463"),
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.add_circle_outline,
+                            color: Colors.white,
+                            size: 30.0,
+                          ),
+                          onPressed: () {},
+                        ),
+                        Text(
+                          '2',
+                          style: GoogleFonts.alata(
+                            textStyle: TextStyle(
+                              // letterSpacing: 2,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              color: HexColor("#FFFFFF"),
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.add_circle_outline,
+                            color: Colors.white,
+                            size: 30.0,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Add to Cart',
+                        style: GoogleFonts.alata(
+                          textStyle: TextStyle(
+                            // letterSpacing: 2,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w300,
+                            color: HexColor("#FFFFFF"),
+                          ),
+                        ),
+                      ),
+                      Icon(
+                        Icons.shopping_cart_outlined,
+                        color: Colors.white,
+                        size: 20.0,
+                      ),
+                    ],
+                  ),
+                  style: TextButton.styleFrom(
+                    elevation: 4.0,
+                    backgroundColor: HexColor("#F74463"),
+                    padding: EdgeInsets.symmetric(
+                        vertical: MediaQuery.of(context).size.height * 0.024,
+                        horizontal: MediaQuery.of(context).size.width * 0.1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                  ),
                 ),
               ],
             ),
