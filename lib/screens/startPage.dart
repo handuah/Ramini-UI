51752c615c222b01734b3d87a15dd39c2d0ca0b5
handuah <handuah@gmail.com>
2021-07-19 16:22:09 +0000

    created animations

--- lib/screens/startPage.dart
+++ lib/screens/startPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 // import 'package:sizer/sizer.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:flutter_animator/flutter_animator.dart';
 
 class StartPage extends StatefulWidget {
   // const StartPage({ Key? key }) : super(key: key);
@@ -19,19 +20,21 @@ class _StartPageState extends State<StartPage> {
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Center(
-            child: Material(
-              elevation: 4.0,
-              borderRadius: BorderRadius.circular(100.0),
-              child: CircleAvatar(
-                maxRadius: 50.0,
-                child: Text(
-                  'R',
-                  style: GoogleFonts.poppins(
-                    textStyle: TextStyle(
-                      fontSize: 50.0,
-                      fontWeight: FontWeight.bold,
-                      color: HexColor("#FFFFFF"),
+          FadeInDown(
+            child: Center(
+              child: Material(
+                elevation: 4.0,
+                borderRadius: BorderRadius.circular(100.0),
+                child: CircleAvatar(
+                  maxRadius: 50.0,
+                  child: Text(
+                    'R',
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.bold,
+                        color: HexColor("#FFFFFF"),
+                      ),
                     ),
                   ),
                 ),
@@ -41,70 +44,76 @@ class _StartPageState extends State<StartPage> {
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.01,
           ),
-          Text(
-            'RAMINI',
-            style: GoogleFonts.poppins(
-              textStyle: TextStyle(
-                letterSpacing: 2,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                color: HexColor("#FFFFFF"),
+          FadeInUp(
+            child: Text(
+              'RAMINI',
+              style: GoogleFonts.poppins(
+                textStyle: TextStyle(
+                  letterSpacing: 2,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: HexColor("#FFFFFF"),
+                ),
               ),
             ),
           ),
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.34,
           ),
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              'Get Started',
-              style: GoogleFonts.poppins(
-                textStyle: TextStyle(
-                  // letterSpacing: 2,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w600,
-                  color: HexColor("#F74463"),
+          FadeInDown(
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                'Get Started',
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                    // letterSpacing: 2,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                    color: HexColor("#F74463"),
+                  ),
                 ),
               ),
-            ),
-            style: TextButton.styleFrom(
-              backgroundColor: HexColor("#FFFFFF"),
-              padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.24,
-                vertical: MediaQuery.of(context).size.height * 0.016,
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
+              style: TextButton.styleFrom(
+                backgroundColor: HexColor("#FFFFFF"),
+                padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.24,
+                  vertical: MediaQuery.of(context).size.height * 0.016,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
               ),
             ),
           ),
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.02,
           ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/signinScreen');
-            },
-            child: Text(
-              'Sign In',
-              style: GoogleFonts.poppins(
-                textStyle: TextStyle(
-                  // letterSpacing: 2,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w600,
-                  color: HexColor("#FFFFFF"),
+          FadeInUp(
+            child: TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/signinScreen');
+              },
+              child: Text(
+                'Sign In',
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                    // letterSpacing: 2,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                    color: HexColor("#FFFFFF"),
+                  ),
                 ),
               ),
-            ),
-            style: TextButton.styleFrom(
-              backgroundColor: HexColor("#F74463"),
-              padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.24,
-                vertical: MediaQuery.of(context).size.height * 0.016,
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
+              style: TextButton.styleFrom(
+                backgroundColor: HexColor("#F74463"),
+                padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.24,
+                  vertical: MediaQuery.of(context).size.height * 0.016,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
               ),
             ),
           ),
